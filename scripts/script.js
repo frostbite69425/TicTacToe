debbd28a48ba5dfeb443f0353f19620afd7f6feb
frostbite69425 <medhihirak3@gmail.com>
2026-01-14 20:02:14 +0530

    add: basic wincon w/o input sanitisation

--- scripts/script.js
+++ scripts/script.js
@@ -40,18 +40,33 @@ const gameBoard = (function gameBoard() {
     player = {
       name,
       symbol,
+      points: 0,
     };
     playersArray.push(player);
   };
 
-  return { getBoard, getPlayers, assignValue, setPlayers };
+  const getPlayerPoints = (player) => {
+    return player.points;
+  };
+
+  const incPlayerPoints = (player) => {
+    player.points = player.points++;
+  };
+
+  return {
+    getBoard,
+    getPlayers,
+    assignValue,
+    setPlayers,
+    getPlayerPoints,
+    incPlayerPoints,
+  };
 })();
 
 function gameStateController() {
   const playerone = gameBoard.setPlayers("Hi", "x");
   const playerTwo = gameBoard.setPlayers("Ji", "o");
   const playerArray = gameBoard.getPlayers();
-  const displayController = tempDisplayController();
   let round = 1;
 
   let activePlayer = playerArray[0];
@@ -66,25 +81,71 @@ function gameStateController() {
   const playRound = () => {
     log(`Round: ${round}`);
     log(`${activePlayer.name}'s turn: `);
-    displayController.tempPromptInput(activePlayer);
+    tempDisplayController.tempPromptInput(activePlayer);
     round++;
 
     let board = gameBoard.getBoard();
 
     // WIN CON CHECK HERE
+    if (round >= 5) {
+      let firstLateralRow = board[0];
+      let secondLateralRow = board[1];
+      let thirdLateralRow = board[2];
+
+      let firstDiagonalRow = [board[0][0], board[1][1], board[2][2]];
+      let secondDiagonalRow = [board[0][2], board[1][1], board[2][0]];
+
+      let firstColumn = [board[0][0], board[1][0], board[2][0]];
+      let secondColumn = [board[0][1], board[1][1], board[2][1]];
+      let thirdColumn = [board[0][2], board[1][2], board[2][2]];
+
+      let winConCheckArray = [
+        firstLateralRow,
+        secondLateralRow,
+        thirdLateralRow,
+        firstDiagonalRow,
+        secondDiagonalRow,
+        firstColumn,
+        secondColumn,
+        thirdColumn,
+      ];
+
+      let result = tempDisplayController.winConController(winConCheckArray);
+
+      if (result) {
+        log(`${activePlayer.name} wins!`);
+        gameBoard.incPlayerPoints(activePlayer);
+        gameBoard.getPlayerPoints(activePlayer);
+      }
+    }
   };
 
   return { playRound, switchPlayer };
 }
 
-const tempDisplayController = () => {
+const tempDisplayController = (() => {
   const tempPromptInput = (activePlayer) => {
     let row = prompt("Row?");
     let column = prompt("column?");
     gameBoard.assignValue(activePlayer, row, column);
   };
 
-  return { tempPromptInput };
-};
+  const winConController = (array) => {
+    let winCon = false;
+    for (row of array) {
+      if (row[0] == "x" || row[0] == "o") {
+        let checkValue = row[0];
+        if (row.filter((cellValue) => cellValue == checkValue).length == 3) {
+          return (winCon = true);
+        }
+      } else {
+        continue;
+      }
+    }
+    return winCon;
+  };
+
+  return { tempPromptInput, winConController };
+})();
 
 let game = gameStateController();
